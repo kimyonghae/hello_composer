2e78a9d283a052aaba88874da1cc59f545582365
yong <dev1234yong@gmail.com>
2018-09-28 10:58:38 +0900

    composer lib생성

---
+++ src/HolloYong.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User:
+ * Date: 2018-09-28
+ * Time: 오전 10:27
+ */
+class HolloYong
+{
+    public function hello()
+    {
+        $txt = '
+                안녕 하세요?!(Ahn-Nyeong Ha-Seh Yo?!)
+                ﻿Good Morning!(영어)< 굿 모닝>
+                ﻿Bon Jour!(불어)<봉 주르>
+                ﻿Buenos Dias!(스페인어)<부에노스 디아스>
+                ﻿Buon giorno!(이태리어)<본 조르노>
+                ﻿Guten Morgen!(독일어)<구텐 모르겐>
+                Zhao Xiang Hao!(중국어)<자우 샹 하오>
+                O-haio gozaimas!(일본어)<오- 하이오 고자이마스>
+                Ah-Saalaam Alah-Ikhoom!(아랍어)<아-쌀람 알라-이쿰>
+                ﻿Goedemorgen!(네덜란드어)<괴데 모르겐>
+                Godmorgen!(덴마크어)<고드 모르겐>
+                Hyaa huomenta!(핀란드어)(히야 후오멘타>
+                Kahleemehrah!(그리스어)<깔리메라>
+                God morgen!(노르웨이어)<고드 모르겐>
+                Dzien dobrey!(폴란드어)(드지엔 도브레이>
+                Bom dia!(포르투갈어)<본 디아>
+                Dobrahyee ootro!(러시아어)<도브라이 우트로>
+                Dobra jutro!(세르비아-크로아티아어)<도브라 주트로>
+                God morgen!(스웨덴어)<고드 모르겐>
+                Swadi Khap!(태국어)<스와디 캅>
+                Ahppa-kkahbah!(말레이-인도네시아어)<아빠-까바>
+                Mwabuka buti!(아프리카어)<므와브카 브티>
+                Mulumele!(잠비아, 짐바브웨, 보츠와나어)<물루멜레>        
+                ';
+         echo $txt;
+    }
+}
